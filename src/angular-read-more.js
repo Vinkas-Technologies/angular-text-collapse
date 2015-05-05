0131e9162571dc8766ddbbd27be75792c1dd3f69
VinothKannanS <vinothkannan@vinkas.com>
2015-05-06 04:31:39 +0530

    Update angular-read-more.js

--- src/angular-read-more.js
+++ src/angular-read-more.js
@@ -1,1 +1,31 @@
-angular.module('angular-carousel', []);
+angular.module('angular-carousel')
+
+.directive('readMore', ['$window', function($window){
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: '<div class="read-more"><div ng-transclude></div><a href ng-click="toggleReadMore()" class="folded-read-more"></a></div>',
+    link: function (scope, element, attrs) {
+      
+      var content = element.find('.read-more');
+      var toggleReadMore = function () {
+        if(content.hasClass('folded-read-more'))
+          content.removeClass('folded-read-more').addClass('unfolded-read-more');
+        else if(content.hasClass('folded-read-more'))  
+          content.removeClass('unfolded-read-more').addClass('folded-read-more');
+        return;
+      };
+      $scope.toggleReadMore = toggleReadMore;
+      
+      var init = function () {
+        var contentHeight = content.outerHeight();
+        if(contentHeight > 48) {
+          content.addClass('folded-read-more');
+          content.show();
+        }
+      };
+      init();
+      
+    }
+  };
+}]);
